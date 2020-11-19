804d253ca71802cf71d56c59e59e6b6b45553bbf
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-19 03:45:35 +0000

    feat(non interactive mode): add non interactive execute of the shell

--- tests/prototype/newshell/shell.c
+++ tests/prototype/newshell/shell.c
@@ -8,37 +8,44 @@
  */
 int main(int ac, char **av)
 {
-	int characters = 0; /* checks for EOF */
+	int characters = 0, exit = 0, glcount = 0, i = 0;
 	char *promt_sign = "$ ";
 	size_t arguments_size = 0;
 	char *arguments = NULL;  /* stores intial buffer */
 	char **arg_array = NULL; /* stores array with arguments */
-	int error = 0;           /* handle errors for syscalls*/
-	int buffer_status = 0;   /* status of buffer, - 1 empty, 0 full*/
-	int exit = 0, glcount = 0;
 
-	while (characters != EOF)
+	if (ac > 1)
 	{
-		glcount++;
-		error = write(STDOUT_FILENO, promt_sign, _strlen(promt_sign));
-		check_error(error);
-		characters = getline(&arguments, &arguments_size, stdin);
-		if (characters == EOF)
-			break;
-		if ((exit = check_exit(arguments)) == -1)
-		{
-			free(arguments);
-			break; /* exit if typed "exit" */
-		}
-		buffer_status = get_arguments(&arguments, &arg_array);
-		if (buffer_status != -1)
-		{
-			execute(arg_array, arguments, glcount, av);
-			free_arguments(&arg_array, &arguments);
-		}
+		arg_array = malloc(sizeof(arg_array) * ac);
+		for (; i < ac - 1; i++)
+			arg_array[i] = av[i + 1];
+		arg_array[i] = NULL;
+		execute(arg_array, NULL, glcount, av);
+		free(arg_array);
 	}
-	if (exit != -1)
-		error = write(STDOUT_FILENO, "\n", 1);
-	check_error(error);
+	else
+	{
+		while (characters != EOF)
+		{
+			glcount++;
+			check_error(write(STDOUT_FILENO, promt_sign, _strlen(promt_sign)));
+			characters = getline(&arguments, &arguments_size, stdin);
+			if (characters == EOF)
+				break;
+			exit = check_exit(arguments);
+			if (exit == -1)
+			{
+				free(arguments);
+				break; /* exit if typed "exit" */
+			}
+			if (get_arguments(&arguments, &arg_array) != -1)
+			{
+				execute(arg_array, arguments, glcount, av);
+				free_arguments(&arg_array, &arguments);
+			}
+		}
+		if (exit != -1)
+			check_error(write(STDOUT_FILENO, "\n", 1));
+	}
 	return (0);
 }
