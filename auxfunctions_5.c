fd64c0b4e77f512d0473fee1a9e85549fb598931
sebastian <sebastian.calleu@gmail.com>
2020-11-25 22:36:41 -0500

    refactor(aux5): adding the code.

--- auxfunctions_5.c
+++ auxfunctions_5.c
@@ -1,2 +1,49 @@
 #include "holberton.h"
 
+/**
+ * freearray - this function frees and array of strings.
+ * @arg_array: the array of strings.
+ */
+
+void freearray(char **arg_array)
+{
+	int freec;
+
+	for (freec = 0; arg_array[freec]; freec++)
+		free(arg_array[freec]);
+	free(arg_array);
+}
+
+/**---------- HELPER */
+/**
+ * check_error - checks status of error
+ * @error: number to check
+ * Description: if error is -1 then print
+ * error message and errno description of
+ * the error
+ *
+ * Return: nothing
+ */
+void check_error(int error)
+{
+	if (error == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+}
+
+/**
+ * envprint - this function prints the environment.
+ */
+
+void envprint(void)
+{
+	int a;
+
+	for (a = 0; environ[a] != NULL; a++)
+	{
+		write(1, environ[a], _strlen(environ[a]));
+		write(1, "\n", 1);
+	}
+}
